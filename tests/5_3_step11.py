6907cb6af388fd351fb868845141bbe94df99d5c
Oldman1958 <boris_i@bk.ru>
2025-10-02 11:46:05 +0500

    Решение заданий

---
+++ tests/5_3_step11.py
@@ -0,0 +1,12 @@
+"""
+В переменную starts_with присвойте lambda функцию, которая принимает строку и возвращает True,
+когда переданная строка начинается с буквы «W».
+Во всех остальных случаях нужно возвращать False
+
+Ничего кроме создания переменной starts_with делать не нужно
+"""
+
+starts_with = lambda s: True if s.startswith('W') else False
+
+print(starts_with('when'))
+print(starts_with.__name__)
---
+++ tests/5_3_step12.py
@@ -0,0 +1,19 @@
+"""
+В переменную check_word присвойте lambda функцию, которая принимает строку и возвращает True,
+если переданная строка начинается с букв «Q» или «R» и заканчивается любой из гласных «A», «E», «I», «U» или «O».
+Регистр во время проверок не должен иметь значения
+
+Во всех остальных случаях нужно возвращать False
+
+Ничего кроме создания переменной check_word делать не нужно
+"""
+
+check_word = lambda s: True if s[0] in ('QRqr') and s[-1] in ('AEIUOaeiuo') else False
+
+print(check_word.__name__)
+print(check_word('radio'))
+
+print(check_word('raid'))
+print(check_word.__name__)
+
+print(check_word('revenge'))
