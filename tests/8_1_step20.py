edf4dc08235988714f20d130c1a18c696c5f9b01
Oldman1958 <boris_i@bk.ru>
2026-08-25 09:46:21 +0500

    Функция range - 3

--- tests/8_1_step20.py
+++ tests/8_1_step20.py
@@ -16,4 +16,29 @@ c шагом step( может быть отрицательное значени
 
 
 def my_range_gen(*args):
-    ...
+    
+    # Распаковка аргументов в start, stop, step
+    if len(args) == 1:
+        start, stop, step = 0, args[0], 1
+    elif len(args) == 2:
+        start, stop, step = args[0], args[1], 1
+    else:  # len(args) == 3
+        start, stop, step = args
+
+    # Генерация значений с учётом направления шага
+    if step > 0:
+        while start < stop:
+            yield start
+            start += step
+    else:
+        while start > stop:
+            yield start
+            start += step
+
+
+for i in my_range_gen(5):
+    print(i)
+for i in my_range_gen(10, 20):
+    print(i)
+for i in my_range_gen(10, 30, 3):
+    print(i)
